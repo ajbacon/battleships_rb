ac8ffe813cbc7a275c23617a96022f10ae89bcff
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-30 22:24:32 +0000

    feat: intitialse game with player. test: tests to pass feature

---
+++ lib/battleships.rb
@@ -0,0 +1,11 @@
+class Battleships
+
+  attr_reader :player1
+
+  def initialize(player = Player.new)
+    @player1 = player
+  end
+
+
+  
+end
--- spec/feature/player_can_view_own_empty_grid_spec.rb
+++ spec/feature/player_can_view_own_empty_grid_spec.rb
@@ -1,9 +1,10 @@
+require 'battleships'
 
 describe "player can place a ship" do
   
   it "should show an empty grid for player 1" do
-    game = Game.new
-    game.start_game
+    game = Battleships.new
+    game.start
   
     expectation = 10.times {'- - - - - - - - - -\n'}
     expect { game.player1.show_grid }.to output(expectation)
